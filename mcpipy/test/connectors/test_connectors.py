c4ac86eec896d3d2311affd380e5399ad8f4a855
Celsa Diaz <celsadiaz@Celsas-Air.fritz.box>
2017-06-25 23:07:59 +0200

    clean up

--- mcpipy/test/connectors/test_connectors.py
+++ mcpipy/test/connectors/test_connectors.py
@@ -3,10 +3,5 @@ from unittest.mock import Mock, patch
 import json
 
 def test_minecraft_connector():
-    # with patch('mcpipy.mcpi.vpython_minecraft.Minecraft') as ws_return_mock, \
-    #        open(PATH_FIXTURES + 'response_gmaps_reverse_geocode.json', 'r') as mock_file:
-    #    minecraft_server_mock = Mock()
-    #    minecraft_server_mock.reverse_geocode.return_value = json.load(mock_file)
-    #    ws_return_mock.return_value = minecraft_server_mock
 
     mc = Minecraft()
